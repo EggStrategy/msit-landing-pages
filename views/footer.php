c62fc088c70827d904266d04b7ad02d87f877961
Brad Sawicki <git@bradsawicki.com>
2014-05-14 15:56:01 -0500

    radio hover animate update

--- views/footer.php
+++ views/footer.php
@@ -5,6 +5,7 @@
 	</footer>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="/assets/js/plugins.js"></script>
 <script src="/assets/js/scripts.js"></script>
 </body>
 </html>

da1a6153cde423cca6ba2db4466722b1196c23bb
Brad Sawicki <git@bradsawicki.com>
2014-05-16 10:45:59 -0500

    Home page icon/title

--- index.php
+++ index.php
@@ -1,107 +1,28 @@
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<title>The Big Soap Box</title>
+	<link href="/assets/css/application.css" rel="stylesheet" media="all">
+	<link rel="shortcut icon" href="/images/favicon.png" />
+	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+
+<body class="page-home">
 
 	<section id="main" class="main">
 		<div class="wrapper cf">
   		
-  		<form action="">
+      <div class="icon-circle-4"></div>
+      <div class="content">
+        <p>The Big Soap Box</p>
+      </div>
 
-        <div class="form-item">
-          <div class="form-question-count">
-            <div class="question-number">01</div>
-            <div class="question-total">(of 3)</div>
-          </div>
-          <div class="icon-circle-1"></div>
-  				<p class="form-question">When was the last time you visited a Hard Rock Cafe?</p>
-  				<div class="form-answers">
-  				  <div class="form-group">
-      				<div class="form-radio">
-                <input type="radio" name="question_1" id="past-month" value="Past Month">
-      				  <label for="past-month"><span>Within the past month</span></label>
-      				</div>
-      				<div class="form-radio">
-                <input type="radio" name="question_1" id="three-months" value="Three Months">
-      				  <label for="three-months"><span>Within the past 3 months</span></label>
-      				</div>
-      				<div class="form-radio">
-                <input type="radio" name="question_1" id="past-year" value="Past Year">
-      				  <label for="past-year"><span>Within the past year</span></label>
-      				</div>
-      				<div class="form-radio">
-                <input type="radio" name="question_1" id="over" value="Over">
-      				  <label for="over"><span>Over a year ago</span></label>
-      				</div>
-      				<div class="form-radio">
-                <input type="radio" name="question_1" id="never" value="Never">
-      				  <label for="never"><span>Never</span></label>
-      				</div>
-  				  </div>
-  				</div>
-        </div>
-
-        <div class="form-item">
-          <div class="form-question-count">
-            <div class="question-number">02</div>
-            <div class="question-total">(of 3)</div>
-          </div>
-          <div class="icon-circle-2"></div>
-  				<p class="form-question">Have you recently recommended Hard Rock Cafe to a friend or family member? </p>
-  				<div class="form-answers form-two-options">
-  				  <div class="form-group">
-      				<div class="form-radio">
-                <input type="radio" name="question_2" id="q2-yes" value="Yes">
-      				  <label for="q2-yes"><span>Yes</span></label>
-      				</div>
-      				<div class="form-radio">
-                <input type="radio" name="question_2" id="q2-no" value="No">
-      				  <label for="q2-no"><span>No</span></label>
-      				</div>
-  				  </div>
-  				</div>
-        </div>
-
-        <div class="form-item">
-          <div class="form-question-count">
-            <div class="question-number">03</div>
-            <div class="question-total">(of 3)</div>
-          </div>
-          <div class="icon-circle-3"></div>
-  				<p class="form-question">Did you share anything about your last experience at a Hard Rock Cafe with a friend, family member or online (e.g. Facebook)?</p>
-  				<div class="form-answers form-two-options">
-  				  <div class="form-group">
-      				<div class="form-radio">
-                <input type="radio" name="question_3" id="q3-yes" value="Yes">
-      				  <label for="q3-yes"><span>Yes</span></label>
-      				</div>
-      				<div class="form-radio">
-                <input type="radio" name="question_3" id="q3-no" value="No">
-      				  <label for="q3-no"><span>No</span></label>
-      				</div>
-  				  </div>
-  				</div>
-        </div>
-				
-				<input type="hidden" name="page_id" value="1">
-        
-        <div class="form-footer">
-          <div class="form-message">
-            <p>That's it!</p>
-          </div>
-  			  <button class="btn">Submit <span class="icon-arrow"></span></button>
-        </div>
-        
-  		</form>
-  		
 		</div>
 	</section>
 
-	<div class="modal-thank-you">
-	  <div class="wrapper">
-      <div class="icon-circle-4"></div>
-      <div class="content">
-        <h2>Thank You!</h2>
-        <p><a href="https://www.google.com">Back to Google search <span class="icon-arrow-grey"></span></a></p>
-      </div>
-	  </div>
-	</div>
-
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
+</body>
+</html>
